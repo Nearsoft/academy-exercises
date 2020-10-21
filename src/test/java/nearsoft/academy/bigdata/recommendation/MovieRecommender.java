6e9f3c2909de3b79e9ba6300b341d772ed7efe66
mayralgr <ml17ft@gmail.com>
2020-10-21 15:00:40 -0500

    Change of comments and consistency of spaces, brackets and comments.

--- src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
+++ src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
@@ -1,11 +1,11 @@
 package nearsoft.academy.bigdata.recommendation;
-/**
- * Big Data Exercises
- * MovieRecommender Uses Amazon movie reviews sample data stanford.edu/data/web-Movies.html for a simple movie recommender
- *
- * by Mayra Lucero Garcia Ramirez
- *
- * Encora Academy 2020B
+/*
+  Big Data Exercises
+  MovieRecommender Uses Amazon movie reviews sample data stanford.edu/data/web-Movies.html for a simple movie recommender
+
+  by Mayra Lucero Garcia Ramirez
+
+  Encora Academy 2020B
  */
 
 // imports from mahout to recommendations
@@ -35,22 +35,19 @@ public class MovieRecommender {
      * Variables used through the program
      */
     private String pathFile     = "";   // path of the gz file
-    private int totalReviews    = 0;    // integer for total of reviews
-    private int totalProducts   = 0;    // integer for total of products
-    private int totalUsers      = 0;    // integer for total of users
-
-    Hashtable<String, Integer> products;    // Hashtable for products
-    Hashtable<String, Integer> users;       // Hashtable for users
-    String currentUser      =   "";         // string for current user, to add it to the csv file
-    String currentProduct   =   "";         // string for current product, to add it to the csv file
-    String currentScore     =   "";         // string for current score, to add it to the csv file
+    private int totalReviews    = 0;
+    private int totalProducts   = 0;
+    private int totalUsers      = 0;
+    Hashtable<String, Integer> products;
+    Hashtable<String, Integer> users;
+    String currentUser      =   "";         // to add the current user to the csv file
+    String currentProduct   =   "";         // to add the current product to the csv file
+    String currentScore     =   "";         // to add the current score to the csv file
 
     /**
-     *
      * @param urlPathToFile receives the path to the gz file
      */
-    MovieRecommender(String urlPathToFile)
-    {
+    MovieRecommender(String urlPathToFile) {
         // initialization of variables
         this.pathFile = urlPathToFile;
         this.users = new Hashtable<String, Integer>();
@@ -58,12 +55,10 @@ public class MovieRecommender {
         this.totalReviews = 0;
         this.totalProducts = 0;
         this.totalUsers = 0;
-        // call to the method that processes the file
-        try
-        {
+        try {
             this.processFile();
-        }catch (IOException exception)
-        {
+        }
+        catch (IOException exception) {
             exception.getStackTrace();
         }
     }
@@ -71,13 +66,18 @@ public class MovieRecommender {
     /**
      * Restart variables used for construct the csv file
      */
-    private void restartVariables()
-    {
+    private void restartVariables() {
         this.currentUser      = "";
         this.currentProduct   = "";
         this.currentScore     = "";
     }
 
+    /**
+     * ProcessFile reads the gz file line by line and search
+     * for the users, products, reviews and scores and calls the method
+     * writeToDataFile to write on the csv file the current user, product and score
+     * @throws IOException
+     */
     private void processFile() throws IOException {
         // variables for gzip
         FileInputStream fileIn = new FileInputStream(this.pathFile);
@@ -89,78 +89,63 @@ public class MovieRecommender {
         File dataForRecommendations = new File("dataForRecommendations.csv");
         FileWriter fileWriter = new FileWriter(dataForRecommendations);
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-
-        // Construction of regex for identify users, reviews, products and scores
+        // construction of regex for identify users, reviews, products and scores
         // \d matches a digit (equal to [0-9])
         // \D matches any character that's not a digit (equal to [^0-9])
-
-        // product
         Pattern productPattern = Pattern.compile("product\\/productId: ([\\D\\d]+)");
-        // users and reviews
         Pattern userPattern = Pattern.compile("review\\/userId: ([\\D\\d]+)");
-        // score for data recommendations
         Pattern scorePattern = Pattern.compile("review\\/score: ([\\D\\d]+)");
-        // declaration of matcher
         Matcher matcher;
         // begin of processing
         while ((line = br.readLine()) != null) {
-            // Product identification
+            // product identification
             matcher = productPattern.matcher(line);
-            if(matcher.matches())
-            {
-                // product
+            if(matcher.matches()) {
                 currentProduct = matcher.group(1);
-                // if it is not already in the list of products it is added and the counter update
-                if(!products.containsKey(currentProduct))
-                {
+                // if it is not already in the list of products it is added and the counter is updated
+                if(!products.containsKey(currentProduct)) {
                     totalProducts +=1;
                     products.put(currentProduct,totalProducts);
                 }
             }
-            // User identification
+            // user identification
             matcher = userPattern.matcher(line);
-            if(matcher.matches())
-            {
-                // user
+            if(matcher.matches()) {
                 currentUser = matcher.group(1);
-                // if it is not already in the list of user it is added and the counter update
-                if(!users.containsKey(currentUser))
-                {
+                // if it is not already in the list of users it is added and the counter is updated
+                if(!users.containsKey(currentUser)) {
                     totalUsers +=1;
                     users.put(currentUser, totalUsers);
                 }
                 // it indicates the existence of a review, the counter is updated
                 totalReviews+=1;
             }
-            // Score identification
+            // score identification
             matcher = scorePattern.matcher(line);
             if(matcher.matches()) {
                 currentScore = matcher.group(1);
             }
-            //write info for recommendations
             writeToDataFile(bufferedWriter);
         }
-        // close the buffers and the file
+        // close the buffers (reader and writer) and the gz file
         bufferedWriter.close();
         br.close();
         gZIPInputStream.close();
     }
 
     /**
-     * Wirte to data file for rrecommendations
+     * Write to data file for recommendations
      * @param bufferedWriter for write to data file
      * @throws IOException
      */
-    private void writeToDataFile(BufferedWriter bufferedWriter) throws IOException{
+    private void writeToDataFile(BufferedWriter bufferedWriter) throws IOException {
         // checks that all the information is complete
-        if(!currentProduct.equals("") && !currentScore.equals("") && !currentUser.equals(""))
-        {
+        if(!currentProduct.equals("") && !currentScore.equals("") && !currentUser.equals("")) {
             // format to integers because the data model only accepts numbers
             int idUserForData = users.get(currentUser);
             int idProductForData = products.get(currentProduct);
             // write in csv file
             bufferedWriter.write(idUserForData + "," + idProductForData + "," + currentScore + "\n");
-            // restart the variables
             restartVariables();
         }
     }
@@ -181,53 +166,45 @@ public class MovieRecommender {
     }
 
     /**
-     *
      * @return of the total reviews
      */
-    public int getTotalReviews()
-    {
+    public int getTotalReviews() {
         return totalReviews;
     }
 
     /**
-     *
      * @return of the total number of products
      */
-
-    public int getTotalProducts()
-    {
+    public int getTotalProducts() {
         return totalProducts;
     }
 
     /**
-     *
      * @return of the total number of users
      */
-    public int getTotalUsers()
-    {
+    public int getTotalUsers() {
         return totalUsers;
     }
 
     /**
-     *
+     * This methods constructs a data model and process it with similarity neighborhood to find
+     * the recommendations for a user based on its own rating scores.
      * @param user to whom recommendations will be made
      * @return a list of the recommendations of products for the user
      * @throws IOException
      * @throws TasteException
      */
-
     public List<String> getRecommendationsForUser(String user) throws IOException, TasteException {
-        //conversion to the intId used for the csv data
+        //conversion to the intId used in the csv data
         int userIntId = users.get(user);
-        // to handle interaction data.
+        // to handle interaction data
         DataModel model = new FileDataModel(new File("./dataForRecommendations.csv"));
         // the correlation coefficient between their interactions
         UserSimilarity similarity = new PearsonCorrelationSimilarity(model);
         // define which similar users we want to leverage for the recommender
         UserNeighborhood neighborhood = new ThresholdUserNeighborhood(0.1, similarity, model);
-        // create our recommender
         UserBasedRecommender recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
-        // recommender receives the userID follow by the number of recommendations, 3
+        // recommender receives the userID follow by the number of recommendations
         List<RecommendedItem> recommendations = recommender.recommend(userIntId, 3);
         // list of recommendations that is going to be returned
         List<String> recommendationsOutput = new ArrayList<String>();
@@ -236,7 +213,7 @@ public class MovieRecommender {
             int value = (int)recommendation.getItemID();
             // extraction of the key of the product
             String productIdRecommendation = getProductId(value);
-            // append the product recommendation to the recommendations lista
+            // append the product recommendation to the recommendations list
             recommendationsOutput.add(productIdRecommendation);
         }
         return recommendationsOutput;
