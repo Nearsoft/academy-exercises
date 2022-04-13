27b4179a3492fa15c1866cd2cbbc46b8298a07d6
bryan.cervantes <byncvn@gmail.com>
2022-04-13 16:41:53 -0700

    Class MovieRecommender and Methods created

---
+++ src/test/java/nearsoft/academy/bigdata/recommendation/MovieRecommender.java
@@ -0,0 +1,28 @@
+package nearsoft.academy.bigdata.recommendation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieRecommender {
+    public MovieRecommender(String rute) {
+
+    }
+
+    public int getTotalReviews() {
+
+        return 0;
+    }
+
+    public int getTotalProducts() {
+        return 0;
+    }
+
+    public int getTotalUsers() {
+        return 0;
+    }
+
+    public List<String> getRecommendationsForUser(String userId) {
+
+        return new ArrayList<>() ;
+    }
+}
